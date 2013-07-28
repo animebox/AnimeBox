0c167a11a242d91646be171190c89b4fbcd5d63a
Leonardo Tosin <leotosin99@hotmail.com>
2013-07-28 04:17:50 -0300

    Tags do Servidor
    Tag pra funfar no servidor, nada d+

---
+++ Prototype/conf/tagsServer.php
@@ -0,0 +1,13 @@
+<?php
+	#Definições do Sistema
+	date_default_timezone_set('America/Sao_Paulo');
+	#define('UrlPadrao' , "http://www.animebox.com.br");
+	define('UrlPadrao' , "http://animebox.com.br/");
+	
+	#Definições do Banco de Dados
+	define('DB_Host' , "db4free.net");
+	define('DB_Database' , "animebox");
+	define('DB_User' , "animebox");
+	define('DB_Pass' , "@nimebox");
+	
+?>
